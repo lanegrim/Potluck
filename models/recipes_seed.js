f6c81e7cbe221a651a1f6c95cea2023f48ce799f
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-11 14:50:43 -0400

    updated seed data

--- models/recipes_seed.js
+++ models/recipes_seed.js
@@ -17,7 +17,7 @@ module.exports = [
             "In a blender or food processor, pulse the tomatoes, green onions, jalapeno peppers, and cilantro to desired consistency.",
             "Transfer to a bowl, and mix in the lime juice, hot pepper sauce, black pepper, garlic powder, and salt."
         ],
-        type: "Snack",
+        type: "snack",
         duration: "15 mins",
     },
 ]
