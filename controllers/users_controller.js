9dd94f77f79dd258586293d4ba3ab71510762fc7
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-14 10:42:56 -0400

    refactored schema, seed data, and user routes to incorporate profile pictures

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -18,6 +18,9 @@ const User = require('../models/users.js')
 
 users.post('/', (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+    if (req.body.picture === "") {
+        req.body.picture = "https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+    }
     User.create(req.body, (err, createdUser) => {
         User.find({}, (err, foundUser) => {
             if (err) {
