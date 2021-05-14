3d7a47cd8ba5b991f3dd9312cf2e69ff4c0e499c
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-14 12:05:53 -0400

    uppercased type in seed data

--- models/recipes_seed.js
+++ models/recipes_seed.js
@@ -17,7 +17,7 @@ module.exports = [
             "In a blender or food processor, pulse the tomatoes, green onions, jalapeno peppers, and cilantro to desired consistency.",
             "Transfer to a bowl, and mix in the lime juice, hot pepper sauce, black pepper, garlic powder, and salt."
         ],
-        type: "snack",
+        type: "Snack",
         duration: "15 mins",
         owner: "Lane",
         ownerPicture: "https://scontent.fatl1-2.fna.fbcdn.net/v/t1.6435-9/70339777_2802977739753160_5718744049771347968_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=730e14&_nc_ohc=9Sw8PB3MX_wAX8JymEU&_nc_ht=scontent.fatl1-2.fna&oh=9fb312e976dc64486967cdcc467ac87c&oe=60C244EC"
