f7bea3f572a1e315db7f6b21e2de9fbb282a3123
Brandon Wright <thewrightbrandon@gmail.com>
2021-05-15 17:54:47 -0400

    Styled edit form

--- public/js/edit.js
+++ public/js/edit.js
@@ -8,105 +8,95 @@ class Edit extends React.Component {
             <summary>Edit your recipe</summary>
 
             <form className="editRecipe" id={this.props._id} onSubmit={this.props.updateRecipe}>
-
-              <div className="row">
-                <div className="col-4">
-                  <label className="form-label" htmlFor="title">Title</label>
-                  <input className="form-control" type="text" id="title"
-                    onChange={this.props.handleChange} defaultValue={this.props.title} />
+              <fieldset>
+                <div className="row mb-4">
+                  <div className="col-6">
+                    <label className="form-label" htmlFor="title">Title</label>
+                    <input className="form-control" type="text" id="title"
+                      onChange={this.props.handleChange} defaultValue={this.props.title} required />
+                  </div>
+                  <div className="col-6">
+                    <label className="form-label" htmlFor="duration">Duration</label>
+                    <input className="form-control" type="text" id="duration"
+                      onChange={this.props.handleChange} defaultValue={this.props.duration} required />
+                  </div>
                 </div>
-                <div className="col-4">
-                  <label className="form-label" htmlFor="duration">Duration</label>
-                  <input className="form-control" type="text" id="duration"
-                    onChange={this.props.handleChange} defaultValue={this.props.duration} />
+                <div className="row mb-4">
+                  <div className="col-6">
+                    <label className="form-label" htmlFor="type">Type of Recipe</label>
+                    <select className="form-select" type="text" id="type"
+                      onChange={this.props.handleChange} value={this.props.type} required>
+                      <option value="">Choose Recipe Type...</option>
+                      <option value="Main">Main</option>
+                      <option value="Side">Side</option>
+                      <option value="Dessert">Dessert</option>
+                      <option value="Snack">Snack</option>
+                    </select>
+                  </div>
+                  <div className="col-6">
+                    <label className="form-label" htmlFor="image">Image</label>
+                    <input className="form-control" type="text" id="image"
+                      onChange={this.props.handleChange} defaultValue={this.props.image} required />
+                  </div>
                 </div>
-                <div className="col-4">
-                  <label className="form-label" htmlFor="type">Type of Recipe</label>
-                  <select className="form-select" type="text" id="type"
-                    onChange={this.props.handleChange} value={this.props.type}>
-                    <option defaultValue>Choose Recipe Type...</option>
-                    <option value="Main">Main</option>
-                    <option value="Side">Side</option>
-                    <option value="Dessert">Dessert</option>
-                    <option value="Snack">Snack</option>
-                  </select>
+                <div className="row">
+                  <div className="col">
+                    <label className="form-label" htmlFor="ingredients">Ingredients</label>
+                    {this.props.ingredients.map((value, index) => {
+                      let ingredientId = `ingredient-${index}`
+                      return (
+                        <input
+                          key={index}
+                          className="form-control ingredients"
+                          type="text"
+                          data-id={index}
+                          id={ingredientId}
+                          onChange={this.props.handleChange}
+                          value={this.props.ingredients[index]}
+                          required/>
+                      )
+                    })}
+                  </div>
                 </div>
-              </div>
-
-              <div className="row">
-                <div className="col">
-                  <label className="form-label" htmlFor="image">Image</label>
-                  <input className="form-control" type="text" id="image"
-                    onChange={this.props.handleChange} defaultValue={this.props.image} />
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-8">
-                  <label className="form-label" htmlFor="ingredients">Ingredients</label>
-                  {this.props.ingredients.map((value, index) => {
-                    let ingredientId = `ingredient-${index}`
-                    return (
-                      <input
-                        key={index}
-                        className="form-control ingredients"
-                        type="text"
-                        data-id={index}
-                        id={ingredientId}
-                        onChange={this.props.handleChange}
-                        value={this.props.ingredients[index]} />
-                    )
-                  })}
-                </div>
-              </div>
-
-              <br />
-              <div className="row">
-                <div>
-                  <button onClick={this.props.addIngredient} className="btn btn-info">Add Ingredient</button>
+                <div className="dynamicButtons">
+                  <button onClick={this.props.addIngredient}
+                  className="btn addIngredientButton">Add Ingredient</button>
                   {this.props.ingredients.length > 1 ? (
-                    <button onClick={this.props.removeIngredient} className="btn btn-warning">Remove Ingredient</button>
+                    <button onClick={this.props.removeIngredient}
+                    className="btn btn-danger removeIngredientButton">Remove Ingredient</button>
                   ) : null}
                 </div>
-              </div>
-              <br />
-
-              <div className="row">
-                <div className="col-8">
-                  <label className="form-label" htmlFor="methods">Methods</label>
-                  {this.props.methods.map((value, index) => {
-                    let methodId = `method-${index}`
-                    return (
-                      <input
-                        key={index}
-                        className="form-control methods"
-                        type="text"
-                        data-id={index}
-                        id={methodId}
-                        onChange={this.props.handleChange}
-                        value={this.props.methods[index]} />
-                    )
-                  })}
+                <div className="row">
+                  <div className="col">
+                    <label className="form-label" htmlFor="methods">Methods</label>
+                    {this.props.methods.map((value, index) => {
+                      let methodId = `method-${index}`
+                      return (
+                        <input
+                          key={index}
+                          className="form-control methods"
+                          type="text"
+                          data-id={index}
+                          id={methodId}
+                          onChange={this.props.handleChange}
+                          value={this.props.methods[index]}
+                          required />
+                      )
+                    })}
+                  </div>
                 </div>
-              </div>
-
-              <br />
-              <div className="row">
-                <div>
-                  <button onClick={this.props.addMethod} className="btn btn-info">Add Step</button>
+                <div className="dynamicButtons">
+                  <button onClick={this.props.addMethod}
+                  className="btn addMethodButton">Add Step</button>
                   {this.props.methods.length > 1 ? (
-                    <button onClick={this.props.removeMethod} className="btn btn-warning">Remove Step</button>
+                    <button onClick={this.props.removeMethod}
+                    className="btn btn-danger removeMethodButton">Remove Step</button>
                   ) : null}
                 </div>
-              </div>
-
-              <br />
-              <div className="row">
-                <div className="col">
-                  <input className="btn btn-success" type="submit" value="Update Recipe" />
+                <div className="submitDiv">
+                  <input className="btn btn-success submitRecipeButton" type="submit" value="Update Recipe" />
                 </div>
-              </div>
-
+              </fieldset>
             </form>
 
           </details>
